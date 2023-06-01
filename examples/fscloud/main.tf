1e9433eff10da1b21f3b0dc9a59045d3559a30e3
Akash Kumar <akash.kumar@ibm.com>
2023-06-01 22:37:11 +0530

    refactor: added standard plan for basic ex.

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -47,7 +47,7 @@ module "cbr_zone" {
 module "event_streams" {
   source                     = "../../profiles/fscloud"
   resource_group_id          = module.resource_group.resource_group_id
-  es_name                    = "${var.prefix}-es"
+  es_name                    = "${var.prefix}-es-fs"
   plan                       = var.plan
   kms_key_crn                = var.kms_key_crn
   existing_kms_instance_guid = var.existing_kms_instance_guid
