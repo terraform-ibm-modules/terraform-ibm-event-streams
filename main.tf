418b74ee461d743820687c977c19d2601b389b67
Akash Kumar <akash.kumar@ibm.com>
2023-06-28 13:22:32 +0530

    refactor: service_endpoint

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "ibm_resource_instance" "es_instance" {
   }
 
   parameters = {
-    service_endpoints = var.service_endpoints
+    service-endpoints = var.service_endpoints
     throughput        = var.throughput
     storage_size      = var.storage_size
     key_protect_key   = var.kms_key_crn
