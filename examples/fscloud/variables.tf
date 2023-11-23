a43b255c0d697073a4127dc28305f2d5a8065c37
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2023-11-23 14:42:36 +0000

    fix: tests

--- examples/fscloud/variables.tf
+++ examples/fscloud/variables.tf
@@ -57,9 +57,11 @@ variable "topics" {
 variable "existing_kms_instance_guid" {
   description = "The GUID of the Hyper Protect Crypto service in which the key specified in var.kms_key_crn is coming from"
   type        = string
+  default     = "e6dce284-e80f-46e1-a3c1-830f7adff7a9"
 }
 
 variable "kms_key_crn" {
   type        = string
   description = "The root key CRN of a Hyper Protect Crypto Service (HPCS) that you want to use for disk encryption. See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-hpcs&interface=ui for more information on integrating HPCS with Event Streams instance."
+  default     = "crn:v1:bluemix:public:hs-crypto:us-south:a/abac0df06b644a9cabc6e44f55b3880e:e6dce284-e80f-46e1-a3c1-830f7adff7a9:key:76170fae-4e0c-48c3-8ebe-326059ebb533"
 }
