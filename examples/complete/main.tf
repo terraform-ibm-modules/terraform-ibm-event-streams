01b9d3aad669a08b25e5a7f3cdafb5dbf5539448
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-02-15 11:51:07 +0000

    address PR comments

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -57,7 +57,7 @@ module "event_streams" {
   source            = "../../"
   resource_group_id = module.resource_group.resource_group_id
   es_name           = "${var.prefix}-es"
-  kms_key_crn       = var.kms_key_crn
+  # kms_key_crn       = var.kms_key_crn
   schemas           = var.schemas
   tags              = var.resource_tags
   topics            = var.topics
