6581cee1943187ebeac19cd67646f47838b5647d
Shikha Maheshwari <shikhamaheshwari@Shikhas-MacBook-Pro.local>
2024-06-17 18:10:17 +0530

    fix: create KMS key, cross account kms, add prefix input to DA

--- solutions/standard/main.tf
+++ solutions/standard/main.tf
@@ -1,18 +1,69 @@
+#######################################################################################################################
+# Resource Group
+#######################################################################################################################
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
   version                      = "1.1.5"
-  resource_group_name          = var.existing_resource_group == false ? var.resource_group_name : null
-  existing_resource_group_name = var.existing_resource_group == true ? var.resource_group_name : null
+  resource_group_name          = var.use_existing_resource_group == false ? (var.prefix != null ? "${var.prefix}-${var.resource_group_name}" : var.resource_group_name) : null
+  existing_resource_group_name = var.use_existing_resource_group == true ? var.resource_group_name : null
 }
 
+#######################################################################################################################
+# Event Streams Instance
+#######################################################################################################################
 module "event_streams" {
   source            = "../../modules/fscloud"
   resource_group_id = module.resource_group.resource_group_id
-  es_name           = var.es_name
+  es_name           = var.prefix != null ? "${var.prefix}-${var.es_name}" : var.es_name
   region            = var.region
-  kms_key_crn       = var.kms_key_crn
+  kms_key_crn       = local.kms_key_crn
   schemas           = var.schemas
   topics            = var.topics
   tags              = var.resource_tags
   cbr_rules         = var.cbr_rules
+}
+
+#######################################################################################################################
+# KMS Key
+#######################################################################################################################
+
+locals {
+  parsed_existing_kms_instance_crn = var.existing_kms_instance_crn != null ? split(":", var.existing_kms_instance_crn) : []
+  kms_region                       = length(local.parsed_existing_kms_instance_crn) > 0 ? local.parsed_existing_kms_instance_crn[5] : null
+  existing_kms_guid                = length(local.parsed_existing_kms_instance_crn) > 0 ? local.parsed_existing_kms_instance_crn[7] : null
+
+  es_key_ring_name = var.prefix != null ? "${var.prefix}-${var.es_key_ring_name}" : var.es_key_ring_name
+  es_key_name      = var.prefix != null ? "${var.prefix}-${var.es_key_name}" : var.es_key_name
+  kms_key_crn      = var.existing_kms_key_crn != null ? var.existing_kms_key_crn : module.kms[0].keys[format("%s.%s", local.es_key_ring_name, local.es_key_name)].crn
+}
+
+# KMS root key for event streams
+module "kms" {
+  providers = {
+    ibm = ibm.kms
+  }
+  count                       = (var.existing_kms_key_crn != null) ? 0 : 1 # no need to create any KMS resources if an existing key is passed
+  source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
+  version                     = "4.13.2"
+  create_key_protect_instance = false
+  region                      = local.kms_region
+  existing_kms_instance_guid  = local.existing_kms_guid
+  key_ring_endpoint_type      = var.kms_endpoint_type
+  key_endpoint_type           = var.kms_endpoint_type
+  keys = [
+    {
+      key_ring_name         = local.es_key_ring_name
+      existing_key_ring     = false
+      force_delete_key_ring = true
+      keys = [
+        {
+          key_name                 = local.es_key_name
+          standard_key             = false
+          rotation_interval_month  = 3
+          dual_auth_delete_enabled = false
+          force_delete             = true
+        }
+      ]
+    }
+  ]
 }
--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -4,7 +4,20 @@ variable "ibmcloud_api_key" {
   sensitive   = true
 }
 
-variable "existing_resource_group" {
+variable "ibmcloud_kms_api_key" {
+  type        = string
+  description = "The IBM Cloud API key with access to create a root key and key ring in the key management service instance. If the KMS instance is in a different account, specify a key from that account. If not specified, the ibmcloud_api_key variable is used."
+  sensitive   = true
+  default     = null
+}
+
+variable "prefix" {
+  type        = string
+  description = "Optional. The prefix to append to all resources that this solution creates."
+  default     = null
+}
+
+variable "use_existing_resource_group" {
   type        = bool
   description = "Whether to use an existing resource group."
   default     = false
@@ -12,11 +25,11 @@ variable "existing_resource_group" {
 
 variable "resource_group_name" {
   type        = string
-  description = "The name of a new or the existing resource group in which to provision the Event Streams instance."
+  description = "The name of a new or the existing resource group to provision the Event Streams instance. If a prefix input variable is passed, it is prefixed to the value in the `<prefix>-value` format."
 }
 
 variable "es_name" {
-  description = "The name to give the Event Streams instance created by this solution."
+  description = "The name of the Event Streams instance to create. If a prefix input variable is passed, it is prefixed to the value in the `<prefix>-value` format."
   type        = string
 }
 
@@ -58,11 +71,6 @@ variable "topics" {
   default     = []
 }
 
-variable "kms_key_crn" {
-  type        = string
-  description = "The root key CRN of the key management service (Key Protect or Hyper Protect Crypto Services) to use to encrypt the payload data. [Learn more](https://cloud.ibm.com/docs/EventStreams?topic=EventStreams-managing_encryption) about integrating Hyper Protect Crypto Services with Event Streams. Configure an authorization policy to allow the Event Streams service to access the key management service instance with the reader role ([Learn more](https://cloud.ibm.com/docs/account?topic=account-serviceauth)). You can't manage the policy in the same Terraform state file as the Event Streams service instance ([Learn more](https://cloud.ibm.com/docs/EventStreams?topic=EventStreams-managing_encryption#using_encryption))."
-}
-
 ##############################################################
 # Context-based restriction (CBR)
 ##############################################################
@@ -81,4 +89,42 @@ variable "cbr_rules" {
   description = "The list of context-based restriction rules to create."
   default     = []
   # Validation happens in the rule module
+}
+
+########################################################################################################################
+# KMS variables
+########################################################################################################################
+
+variable "existing_kms_instance_crn" {
+  type        = string
+  default     = "crn:v1:bluemix:public:kms:us-south:a/abac0df06b644a9cabc6e44f55b3880e:d4559998-17ab-4ab8-ba9e-a0c9d2c4ef23::"
+  description = "The CRN of the key management service (KMS). If you are not using an existing KMS instance, you must specify this CRN. To support cross account functionality you must also provide a value for `ibmcloud_kms_api_key`."
+}
+
+variable "existing_kms_key_crn" {
+  type        = string
+  default     = null
+  description = "Optional. The CRN of an existing root key in key management service (KMS) to encrypt the payload data in event streams. To create a key ring and key, pass a value for the `existing_kms_instance_crn` input variable. To support cross account functionality you must also provide a value for `ibmcloud_kms_api_key`."
+}
+
+variable "kms_endpoint_type" {
+  type        = string
+  description = "The type of endpoint to use to communicate with the key management service (KMS). Specify one of the following values for the endpoint type: `public` or `private` (default). To support cross account functionality you must also provide a value for `ibmcloud_kms_api_key`."
+  default     = "private"
+  validation {
+    condition     = can(regex("public|private", var.kms_endpoint_type))
+    error_message = "Valid values for the `kms_endpoint_type_value` are `public` or `private`. "
+  }
+}
+
+variable "es_key_ring_name" {
+  type        = string
+  default     = "es-key-ring"
+  description = "The name of the key ring to create for the Event Streams instance. If an existing key is used, this variable is not required. If the prefix input variable is passed, the name of the key ring is prefixed to the value in the `prefix-value` format."
+}
+
+variable "es_key_name" {
+  type        = string
+  default     = "es-cos-key"
+  description = "The name of the key to create for the Event Streams instance. If an existing key is used, this variable is not required. If the prefix input variable is passed, the name of the key is prefixed to the value in the `prefix-value` format."
 }
