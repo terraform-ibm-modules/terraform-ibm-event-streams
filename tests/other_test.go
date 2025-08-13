ee40bb5687824f65305312d6a51271e9010441ce
Jordan <jordanwilliams997@hotmail.com>
2025-08-13 09:18:50 +0100

    test: do not run TestFSCloudInSchematics and TestSecurityEnforcedSolutionInSchematics in parallel to prevent conflicts between authorization policies

--- tests/other_test.go
+++ tests/other_test.go
@@ -162,7 +162,6 @@ func setupSecurityEnforcedOptions(t *testing.T, prefix string) *testschematic.Te
 
 // Test for the SecurityEnforced DA
 func TestSecurityEnforcedSolutionInSchematics(t *testing.T) {
-	t.Parallel()
 	options := setupSecurityEnforcedOptions(t, "es-sen")
 	err := options.RunSchematicTest()
 	assert.Nil(t, err, "This should not have errored")
