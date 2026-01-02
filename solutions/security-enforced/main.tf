c88bcc21743043e6e22608ac6982d62e6959a24d
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-02 20:08:59 +0000

    fix(deps): update dependencies (#509)

--- solutions/security-enforced/main.tf
+++ solutions/security-enforced/main.tf
@@ -105,7 +105,7 @@ module "kms" {
   }
   count                       = var.existing_kms_key_crn == null ? 1 : 0 # no need to create any KMS resources if passing an existing key
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "5.5.16"
+  version                     = "5.5.17"
   create_key_protect_instance = false
   region                      = local.kms_region
   existing_kms_instance_crn   = var.existing_kms_instance_crn
