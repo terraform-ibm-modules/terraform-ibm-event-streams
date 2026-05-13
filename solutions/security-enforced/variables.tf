882882b120304d5e6a18992798e93e31b9e3c53b
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2026-05-13 11:44:06 +0530

    add to the DA

--- solutions/security-enforced/variables.tf
+++ solutions/security-enforced/variables.tf
@@ -146,6 +146,19 @@ variable "delete_timeout" {
   default     = "15m"
 }
 
+variable "storage_size" {
+  type        = number
+  description = "Storage size of the Event Streams in GB. Applies only to Enterprise plan instances. Possible values: `2048`, `4096`, `6144`, `8192`, `10240`, `12288`. Storage capacity cannot be reduced after the instance is created. When the `throughput` input variable is set to `300`, storage size starts at 4096. When `throughput` is `450`, storage size starts starts at `6144`."
+  default     = "2048"
+}
+
+variable "throughput" {
+  type        = number
+  description = "Throughput capacity in MB per second. Applies only to Enterprise plan instances. Possible values: `150`, `300`, `450`."
+  default     = "150"
+}
+
+
 ##############################################################
 # Mirroring
 ##############################################################
