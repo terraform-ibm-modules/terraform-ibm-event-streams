5279e4158aedff956c0e094641666e2072fa742e
raagamounica <mounica.tellabati@ibm.com>
2026-09-17 20:21:01 +0530

    chore: merge main into update-gen2-DA, resolve version.tf provider conflicts

--- solutions/quickstart/version.tf
+++ solutions/quickstart/version.tf
@@ -1,9 +1,14 @@
 terraform {
   required_version = ">= 1.9.0"
+  # Lock DA into an exact provider version - renovate automation will keep it updated
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = " 1.88.0"
+      version = "2.5.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.14.1"
     }
   }
 }
